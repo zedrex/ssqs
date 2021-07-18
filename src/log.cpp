605b0e60f401e8dfde43c8f90693c9df2f893f4e
zedrex <adib14021999@gmail.com>
2021-07-19 01:59:10 +0600

    Changed server status output value to string from boolean

--- src/log.cpp
+++ src/log.cpp
@@ -6,5 +6,5 @@ int Log::line_number = 0;
 
 void Log::CreateRecord(std::string event_type, double time, int customer_serial, ServerStatus server_status, int queue_size)
 {
-    std::cout << ++line_number << "," << event_type << "," << time << "," << customer_serial << "," << (int)server_status << "," << queue_size << std::endl;
+    std::cout << ++line_number << "," << event_type << "," << time << "," << customer_serial << "," << (server_status == ServerStatus::BUSY ? "Busy" : "Idle") << "," << queue_size << std::endl;
 }
