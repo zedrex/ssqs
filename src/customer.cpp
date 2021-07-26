c21589d8ab6d0338211e833ff8f1eddd07cd0f71
zedrex <adib14021999@gmail.com>
2021-07-26 23:33:21 +0600

    Added new logging system and server

--- src/customer.cpp
+++ src/customer.cpp
@@ -6,8 +6,8 @@ Customer::Customer() {}
 
 Customer::Customer(double arrival_time)
 {
-    this->arrival_time = arrival_time;
     this->serial = ++(this->total_customer);
+    this->arrival_time = arrival_time;
 }
 
 int Customer::GetSerial()
@@ -18,4 +18,29 @@ int Customer::GetSerial()
 int Customer::GetTotalCustomers()
 {
     return Customer::total_customer;
+}
+
+void Customer::SetServiceStartTime(double time)
+{
+    this->service_start_time = time;
+}
+
+void Customer::SetDepartureTime(double time)
+{
+    this->departure_time = time;
+}
+
+double Customer::GetArrivalTime()
+{
+    return this->arrival_time;
+}
+
+double Customer::GetServiceStartTime()
+{
+    return this->service_start_time;
+}
+
+double Customer::GetDepartureTime()
+{
+    return this->departure_time;
 }
